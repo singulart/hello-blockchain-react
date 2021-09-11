3a6bf9ccd5716edf2b2a92e27c8056be1c02dc65
singulart <serpian@gmail.com>
2021-09-11 20:22:41 +0300

    CSS Transition

--- src/components/Joystream/Joystream.tsx
+++ src/components/Joystream/Joystream.tsx
@@ -1,42 +1,76 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import styles from './Joystream.module.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./Joystream.module.css";
 import { WsProvider, ApiPromise } from "@polkadot/api";
 import { types } from "@joystream/types";
+import { CSSTransition } from "react-transition-group";
 
-const wsLocation = 'wss://rome-rpc-endpoint.joystream.org:9944'
-const provider = new WsProvider(wsLocation)
+const wsLocation = "wss://rome-rpc-endpoint.joystream.org:9944";
+
+const provider = new WsProvider(wsLocation);
 
 const Joystream = () => {
-
-  const [lastBlock, updateLastBlock] = useState(0)
-  const [connected, updateConnected] = useState(false)
+  const [lastBlock, updateLastBlock] = useState(0);
+  const [connected, updateConnected] = useState(false);
+  const [blockShown, updateBlockShown] = useState(false);
 
   useEffect(() => {
-    ApiPromise.create({ provider, types }).then((api) =>
-      api.isReady.then(() => {
+    if (!connected) {
+      ApiPromise.create({ provider, types }).then((api) =>
+        api.isReady.then(() => {
           console.log(`Connected to ${wsLocation}`);
           api.rpc.chain.subscribeNewHeads((header) => {
             console.log(`Chain is at block: #${header.number}`);
-            
+
             if(!connected) {
               updateConnected(true);
             }
 
-            if(header.number.toNumber() !== lastBlock) {
-             updateLastBlock(header.number.toNumber());
+            if (header.number.toNumber() !== lastBlock) {
+              updateBlockShown(false);
+
+              updateLastBlock(header.number.toNumber());
+
+              updateBlockShown(true);
             }
-          })
-      })
-    );
-  }, [lastBlock, connected]);
+          });
+        })
+      );
+    }
+  }, [
+    lastBlock,
+    connected,
+    blockShown,
+    updateBlockShown,
+    updateConnected,
+    updateLastBlock,
+  ]);
 
   return (
     <div className={styles.Joystream} data-testid="Joystream">
-      {!connected? 'Connecting to blockchain...' : 
-      `Connected! Latest block is: ${lastBlock.toLocaleString()}`}
+      {!connected ? (
+        <div>Connecting to blockchain...</div>
+      ) : (
+        <>
+          <div>Connected! Latest block is: </div>
+          <CSSTransition
+            in={blockShown}
+            classNames={{
+              enterActive: styles.blockHeightEnterActive,
+              enter: styles.blockHeightEnter,
+              exitActive: styles.blockHeightExitActive,
+              exit: styles.blockHeightExit,
+            }}
+            addEndListener={() => {
+              updateBlockShown(true);
+            }}
+          >
+            <h1>{lastBlock.toLocaleString()}</h1>
+          </CSSTransition>
+        </>
+      )}
     </div>
-  )
+  );
 };
 
 export default Joystream;

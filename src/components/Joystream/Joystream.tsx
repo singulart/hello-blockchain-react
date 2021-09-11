8ae4a243d5478c76241ff588c7e220863aff263c
Oleksii Buistov <oleksii@backbase.com>
2021-09-11 20:04:36 +0300

    Api Client reads the last block number from blockchain

--- src/components/Joystream/Joystream.tsx
+++ src/components/Joystream/Joystream.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import styles from './Joystream.module.css';
+import { WsProvider, ApiPromise } from "@polkadot/api";
+import { types } from "@joystream/types";
 
-const Joystream = () => (
-  <div className={styles.Joystream} data-testid="Joystream">
-    Joystream Component
-  </div>
-);
+const wsLocation = 'wss://rome-rpc-endpoint.joystream.org:9944'
+const provider = new WsProvider(wsLocation)
+
+const Joystream = () => {
+
+  const [lastBlock, updateLastBlock] = useState(0)
+  const [connected, updateConnected] = useState(false)
+
+  useEffect(() => {
+    ApiPromise.create({ provider, types }).then((api) =>
+      api.isReady.then(() => {
+          console.log(`Connected to ${wsLocation}`);
+          api.derive.chain.bestNumber().then((finalizedHeadNumber) => {
+            updateLastBlock(finalizedHeadNumber.toNumber())
+            updateConnected(true)
+          }
+        )
+      })
+    );
+  }, [lastBlock, connected]);
+
+  return (
+    <div className={styles.Joystream} data-testid="Joystream">
+      {!connected? 'Connecting to blockchain...' : 
+      `Connected! Latest block is: ${lastBlock.toLocaleString()}`}
+    </div>
+  )
+};
 
 export default Joystream;
